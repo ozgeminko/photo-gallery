63c6c81abbdd06efcfaaca6e5cb48f92b148e085
Ozge Akbulak <ozgekbulak@gmail.com>
2022-04-21 12:02:15 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const panels = document.querySelectorAll(".panel");
+
+console.log(panels);
+
+panels.forEach(function (panel) {
+  panel.addEventListener("click", function () {
+    removeActiveClasses();
+    panel.classList.add("active");
+  });
+});
+
+function removeActiveClasses() {
+  panels.forEach(function (panel) {
+    panel.classList.remove("active");
+  });
+}

4987c55235b0a289c32b79f67cb7525fc961a154
Ozge Akbulak <ozgekbulak@gmail.com>
2022-04-22 11:54:45 +0300

    photos put in a file

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 const panels = document.querySelectorAll(".panel");
 
-console.log(panels);
+document.getElementById("btn1").addEventListener("click", iconİ);
+document.getElementById("btn2").addEventListener("click", iconİi);
+
+const panelCount = panels.length;
+let activePanelId = 0;
 
 panels.forEach(function (panel) {
   panel.addEventListener("click", function () {
+    activePanelId = panel.id;
+
     removeActiveClasses();
     panel.classList.add("active");
   });
@@ -13,4 +19,30 @@ function removeActiveClasses() {
   panels.forEach(function (panel) {
     panel.classList.remove("active");
   });
+}
+
+function iconİ() {
+  if (activePanelId === 0) {
+    activePanelId = (panels.length - 1).toString();
+  } else {
+    activePanelId = +activePanelId - 1;
+  }
+
+  const panel = document.getElementById(activePanelId.toString());
+
+  removeActiveClasses();
+  panel.classList.add("active");
+}
+
+function iconİi() {
+  if (activePanelId === panels.length - 1) {
+    activePanelId = "0";
+  } else {
+    activePanelId = +activePanelId + 1;
+  }
+
+  const panel = document.getElementById(activePanelId.toString());
+
+  removeActiveClasses();
+  panel.classList.add("active");
 }
